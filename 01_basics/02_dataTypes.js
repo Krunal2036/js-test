bd0452f8bbbef9479a7b4473e0c1457e462cfe81
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-10 07:07:35 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name = "ksr"
+let age = 21
+let online = true
+
+
+/*
+    number => 2 to power of 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalon value
+    undefined => 
+    symbol => unique
+
+    object => typeof
+*/
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber);
+
+ /*
+    "33" => number
+    "33abc" => NaN
+    true => 1 ; false => 0
+ */
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+    1 => true ; 0 => false
+    "" => false ;"abc" => true
+ */
+
+let someNumber = "33"
+
+let stringNumber = String(score)
+console.log(stringNumber);
+console.log(typeof stringNumber);
