87a795503ea82ee1237d677fbb1a9ab4044c5a5d
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-10 11:31:21 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// primitive
+// 7 type = string, number, boolean, bigInt, null, undefined, symbol
+
+const number = 100;
+const subnumber = 100.3;
+const string = "hetesh";
+const isLoggedIn = true;
+const temp = null;
+let userEmail;
+const bigInt = 123544n;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+// non primitive (reference)
+// array, object, function
+
+const city = ["rajkot", "ahemdabad", "jamnagar", "morbi", "surat"];
+let obj ={
+    name:"krunal",
+    age: 21,
+}
+
+const myFuntion = function(){
+    console.log("hello world!");
+}
