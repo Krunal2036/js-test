0396f6a16bfb43e0e0480c4352a9042387ae19c5
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-10 06:24:11 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accId = 144556
+let emailId = "krunal@gmail.com"
+var ePassword = "123456"
+city = "rajkot"
+let state;
+
+// accId = 3 // not allowed
+
+emailId = "krunal@g.com"
+ePassword = "123"
+city = "rjk"
+console.log(accId);
+console.table([accId,emailId,ePassword,city,state]);
