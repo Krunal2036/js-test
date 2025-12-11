e8d5a2bb1431a2129688738886d4896014da4f7f
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-11 11:44:34 +0000

    container config done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+const num = 100;
+// const num_demo = new Number(400);
+// console.log(num_demo.toString().length);
+// console.log(num_demo.toFixed(1));
+
+const value = 102.2589
+// console.log(value.toPrecision(4));
+
+//===============================math=====================================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.3));
+// console.log(Math.ceil(3.1));
+// console.log(Math.floor(3.8));
+// console.log(Math.min(4,5,2,8,1,3));
+// console.log(Math.max(4,5,2,8,1,3));
+
+// console.log((Math.random()*10)+1);
+const min = 1;
+const max = 6;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
