182a95f4d23c8c7a8d9cdffe120ec792b5a11787
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-16 07:05:14 +0000

    container config done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,7 @@
+// Immediately Invoked Function Expression (IIFE)
+(function demo(){
+    console.log("DB CONNECTED");
+})();
+(function(name){
+    console.log(`Hello World ${name}`);
+})('Krunal');
