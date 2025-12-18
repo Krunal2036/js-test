cf126502bf7146b447069f60ba12d9e0e84feed4
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-18 05:22:18 +0000

    container config done

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,1 +1,11 @@
- 
+let count = 1;
+while(count <= 5) {
+    console.log(count);
+    count++;
+}
+
+let do_count = 2;
+do {
+    // console.log(do_count);
+    do_count--;
+} while (do_count >= 1);
