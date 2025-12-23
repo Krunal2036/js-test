84c3e934073dd88be2143e58455993ef46d669df
Krunal2036 <110545364+Krunal2036@users.noreply.github.com>
2025-12-23 10:00:08 +0000

    objects done

--- 09_class _and_oop/objects.js
+++ 09_class _and_oop/objects.js
@@ -3,7 +3,27 @@ function multipliByFive(num) {
 }
 multipliByFive.power = 2;
 
-console.log(multipliByFive(5));
-console.log(multipliByFive.power);
-console.log(multipliByFive.prototype);
+// console.log(multipliByFive(5));
+// console.log(multipliByFive.power);
+// console.log(multipliByFive.prototype);
  
+function drinks(name,price) {
+    this.name = name;
+    this.price = price;
+}
+
+drinks.prototype.increasePrice = function(){
+    this.price++;
+}
+
+drinks.prototype.showPrice = function(){
+    console.log(`${this.price} for ${this.name}`);
+}
+
+const chai = new drinks('chai', 20)
+const coffe = new drinks('coffe', 160)
+
+chai.increasePrice();
+chai.showPrice();
+
+coffe.showPrice();
